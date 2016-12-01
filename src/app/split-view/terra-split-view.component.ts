072f1abd0a9604add18fe2c1152f618cbf758b0e
Daniel Trauf <daniel.trauf@plentymarkets.com>
2016-12-01 19:25:06 +0100

    updated draggable

--- src/app/split-view/terra-split-view.component.ts
+++ src/app/split-view/terra-split-view.component.ts
@@ -1,7 +1,9 @@
 import {
     Component,
     Input,
-    DoCheck
+    DoCheck,
+    ElementRef,
+    Inject
 } from '@angular/core';
 import { TerraSplitViewInterface } from './data/terra-split-view.interface';
 import {
@@ -9,6 +11,9 @@ import {
     LocaleService,
     LocalizationService
 } from 'angular2localization';
+
+// declare variable to use jQuery code
+declare var jQuery:any;
 
 @Component({
                selector: 'terra-split-view',
@@ -20,54 +25,97 @@ export class TerraSplitViewComponent extends Locale implements DoCheck
     @Input() inputModules:Array<TerraSplitViewInterface>;
     @Input() inputShowBreadcrumbs:boolean;
     private _isSingleComponent:boolean;
+    private elementRef:ElementRef;
     
-    constructor(public locale:LocaleService,
+    constructor(@Inject(ElementRef) elementRef:ElementRef,
+                public locale:LocaleService,
                 public localization:LocalizationService)
     {
         super(locale, localization);
+        this.elementRef = elementRef;
         this.inputShowBreadcrumbs = true;
     }
     
     ngDoCheck()
     {
-        if(this.inputModules.length > 3)
+        if(this.inputModules)
         {
-            for(let index = this.inputModules.length - 1; index >= 0; index--)
+            if(this.inputModules.length > 3)
             {
-                if(this.inputModules.length - 1 - index < 3)
+                for(let index = this.inputModules.length - 1; index >= 0; index--)
                 {
-                    this.inputModules[index].hidden = false;
+                    if(this.inputModules.length - 1 - index < 3)
+                    {
+                        this.inputModules[index].hidden = false;
+                    }
+                    else
+                    {
+                        this.inputModules[index].hidden = true;
+                    }
                 }
-                else
+            }
+            else
+            {
+                if(this.inputModules[0])
                 {
-                    this.inputModules[index].hidden = true;
+                    this.inputModules[0].hidden = false;
+                }
+                if(this.inputModules[1])
+                {
+                    this.inputModules[1].hidden = false;
+                }
+                if(this.inputModules[2])
+                {
+                    this.inputModules[2].hidden = false;
                 }
             }
-        }
-        else
-        {
-            if(this.inputModules[0])
+            
+            if(this.inputModules.length == 1)
             {
-                this.inputModules[0].hidden = false;
+                this._isSingleComponent = true;
             }
-            if(this.inputModules[1])
+            else
             {
-                this.inputModules[1].hidden = false;
-            }
-            if(this.inputModules[2])
-            {
-                this.inputModules[2].hidden = false;
+                this._isSingleComponent = false;
             }
         }
         
-        if(this.inputModules.length == 1)
-        {
-            this._isSingleComponent = true;
-        }
-        else
-        {
-            this._isSingleComponent = false;
-        }
+        jQuery(this.elementRef.nativeElement).find('.divider').draggable({ axis: "x" });
+        //jQuery(this.elementRef.nativeElement).find('.divider').mousedown(function()
+        //                                                               {
+        //                                                                   jQuery('.side-scroller').addClass("flexible-width");
+        //                                                               });
+        //jQuery(this.elementRef.nativeElement).find('.divider').mouseup(function()
+        //                                                             {
+        //                                                                 jQuery('.side-scroller').removeClass("flexible-width");
+        //                                                             });
+        jQuery(this.elementRef.nativeElement).find('.divider').draggable({
+                                                                             start: function()
+                                                                                    {
+                                                                                        
+                                                                                        
+                                                                                        //jQuery('.divider').each(function(elem)
+                                                                                        //                        {
+                                                                                        //                            elem.setStyle('side-scroller', 'width', 'auto')
+                                                                                        //                        });
+                                                                                    },
+                                                                             drag:  function()
+                                                                                    {
+                                                                                        var p = jQuery( ".divider" );
+                                                                                        var position = p.position();
+                                                                                        //jQuery( ".side-scroller > *" ).text( "left: " + position.left + ", top: " + position.top );
+                                                                                        jQuery(".flexible-width").attr({
+                                                                                                           "style" : "width: " + (position.left / window.innerWidth*100)+'%'
+                                                                                                       });
+                                                                                    },
+                                                                             stop:  function()
+                                                                                    {
+                                                                                        //jQuery('.divider')
+                                                                                        //    .setStyle('side-scroller', 'width', '32.8%');
+                                                                                    }
+                                                                         });
+        jQuery('.divider').position()
+    
     }
     
     private onClick():void
